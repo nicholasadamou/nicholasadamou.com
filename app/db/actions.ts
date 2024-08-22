60d0854fd0f7d8451a1b3275df7be86f3253e0a4
Nicholas Adamou <nicholasadamouemail@gmail.com>
2024-08-22 18:08:21 -0400

    Improve error handling and add logging
    Enhanced `incrementViews` and `getViewsCount` functions with try-catch blocks for better error handling. Added console logs for easier debugging and system monitoring. Updated `.idea/workspace.xml` to reflect new changes and improve code readability and configuration tracking.

--- app/db/actions.ts
+++ app/db/actions.ts
@@ -2,12 +2,16 @@
 import { sql } from "@vercel/postgres";
 
 export async function incrementViews(slug: string) {
-  if (process.env.NODE_ENV === "development") return;
-
-  await sql`
-    INSERT INTO blog_views (slug, count) 
-    VALUES (${slug}, 1) 
-    ON CONFLICT (slug) 
-    DO UPDATE SET count = blog_views.count + 1
-  `;
+	console.log(`incrementViews(${slug})`);
+	try {
+		await sql`
+      INSERT INTO blog_views (slug, count)
+      VALUES (${slug}, 1)
+      ON CONFLICT (slug)
+      DO UPDATE SET count = blog_views.count + 1
+    `;
+		console.log(`Successfully incremented ${slug} by 1 view`);
+	} catch (error) {
+		console.error("Error incrementing views:", error);
+	}
 }
--- app/db/queries.tsx
+++ app/db/queries.tsx
@@ -2,15 +2,20 @@
 import { sql } from "@vercel/postgres";
 
 export async function getViewsCount(): Promise<
-  { slug: string; count: number }[]
+	{ slug: string; count: number }[]
 > {
-  if (!process.env.POSTGRES_URL) {
-    return [];
-  }
+	console.log("getViewsCount()");
+	try {
+		const result = await sql`
+      SELECT slug, count FROM blog_views;
+    `;
 
-  const result = await sql`
-    SELECT slug, count FROM blog_views;
-  `;
+		const results = result.rows as { slug: string; count: number }[];
+		console.log(results);
 
-  return result.rows as { slug: string; count: number }[];
+		return results;
+	} catch (error) {
+		console.error("Error in getViewsCount():", error);
+		return [];
+	}
 }
