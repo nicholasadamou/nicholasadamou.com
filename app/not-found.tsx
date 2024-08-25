a0e4c8700670f3655585e5767ded525d755f796e
Nicholas Adamou <nicholasadamouemail@gmail.com>
2024-08-24 20:23:01 -0400

    Refactor import order and update 404 page text.
    Reordered import statements in `page.tsx` for consistency and edited the 404 page text for clarity and user-friendliness. Also updated the IntelliJ IDEA workspace configuration to reflect these changes.

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,12 +1,14 @@
-import Link from "@/app/components/Link";
+import React from "react";
 import { Metadata } from "next";
+
+import Link from "@/app/components/Link";
 
 export const metadata: Metadata = {
   title: "404 | Nicholas Adamou",
   description: "Uh oh! This page does not exist",
 };
 
-const Custom404 = (): JSX.Element => (
+const Custom404 = (): React.ReactNode => (
 	<div className="flex flex-col gap-2">
 		<h1 className="text-3xl font-bold tracking-tight text-primary">404</h1>
 		<p className="text-secondary max-w-sm">
