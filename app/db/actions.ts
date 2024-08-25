4116edd48d683f19353e54a4f40db94b1d99acc1
Nicholas Adamou <nicholasadamouemail@gmail.com>
2024-08-25 07:35:06 -0400

    Throttle view increments and set last view time cookie
    Added throttling to view increments by checking the last view time in cookies to prevent rapid consecutive increments. Additionally, a new cookie is set to track the last view time, ensuring views are only incremented if a certain time has elapsed.

--- app/db/actions.ts
+++ app/db/actions.ts
@@ -1,9 +1,19 @@
 "use server";
 import { sql } from "@vercel/postgres";
 
-export async function incrementViews(slug: string) {
+export async function incrementViews(slug: string, req: Request) {
 	if (!process.env.POSTGRES_URL) {
 		console.log("No POSTGRES_URL found, skipping incrementViews");
+		return;
+	}
+
+	const cookie = req.headers.get("cookie");
+	const lastViewTime = cookie ? parseInt(cookie.split('lastViewTime=')[1]) : 0;
+	const currentTime = Date.now();
+
+	// Check if the last view was less than a minute ago
+	if (currentTime - lastViewTime < 60000) {
+		console.log("View increment skipped due to throttling.");
 		return;
 	}
 
@@ -11,11 +21,16 @@ export async function incrementViews(slug: string) {
 
 	try {
 		await sql`
-      INSERT INTO blog_views (slug, count)
-			VALUES (${slug}, 1)
-			ON CONFLICT (slug)
-			DO UPDATE SET count = blog_views.count + EXCLUDED.count;
-    `;
+            INSERT INTO blog_views (slug, count)
+            VALUES (${slug}, 1)
+            ON CONFLICT (slug)
+            DO UPDATE SET count = blog_views.count + EXCLUDED.count;
+        `;
+
+		// Set a cookie to track the last view time
+		const newCookie = `lastViewTime=${currentTime}; Path=/; HttpOnly;`;
+		req.headers.set("Set-Cookie", newCookie);
+
 		console.log(`Successfully incremented ${slug} by 1 view`);
 	} catch (error) {
 		console.error("Error incrementing views:", error);
