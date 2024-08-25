342240f56e7b1925b302d4c2ab1e04224d2a27b4
Nicholas Adamou <nicholasadamouemail@gmail.com>
2024-08-24 23:00:20 -0400

    Update post message and clean up blog description
    Changed the message displayed when no blog posts are present and simplified the blog page's introductory paragraph. These adjustments improve clarity and user comprehension.

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -34,30 +34,29 @@ export default function BlogPage() {
   return (
     <div className="flex flex-col gap-16 md:gap-24">
       <div className="flex flex-col gap-8">
-        <div>
-          <h1 className="animate-in text-3xl font-bold tracking-tight">Blog</h1>
-          <p
-            className="mt-5 animate-in text-secondary"
-            style={{ "--index": 1 } as React.CSSProperties}
-          >
-            While I don’t write on a regular basis, I find great value in
-            sharing my insights and learnings whenever the opportunity arises.
-            Each post reflects my thoughts and experiences, and I hope they
-            resonate with others. To date, I’ve published {blogs.length} posts,
-            and I look forward to contributing more in the future.
-          </p>
-        </div>
-      </div>
-      <div
-        className="animate-in"
-        style={{ "--index": 2 } as React.CSSProperties}
-      >
-        <PostList posts={blogs} />
-      </div>
-      <div
-        className="animate-in"
-        style={{ "--index": 3 } as React.CSSProperties}
-      ></div>
-    </div>
-  );
+				<div>
+					<h1 className="animate-in text-3xl font-bold tracking-tight">Blog</h1>
+					<p
+						className="mt-5 animate-in text-secondary"
+						style={{ "--index": 1 } as React.CSSProperties}
+					>
+						While I don’t write on a regular basis, I find great value in
+						sharing my insights and learnings whenever the opportunity arises.
+						Each post reflects my thoughts and experiences, and I hope they
+						resonate with others.
+					</p>
+				</div>
+			</div>
+			<div
+				className="animate-in"
+				style={{ "--index": 2 } as React.CSSProperties}
+			>
+				<PostList posts={blogs} />
+			</div>
+			<div
+				className="animate-in"
+				style={{ "--index": 3 } as React.CSSProperties}
+			></div>
+		</div>
+	);
 }
