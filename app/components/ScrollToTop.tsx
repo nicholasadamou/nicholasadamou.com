19ea2afd92ec26375e972d147fc732dc963b94de
Nicholas Adamou <nicholasadamouemail@gmail.com>
2024-08-26 06:56:46 -0400

    Add responsive visibility for ScrollToTop component
    The ScrollToTop button is now hidden on large screens to improve UI responsiveness and aesthetics. This change ensures the button only appears on smaller screens where it is more likely to be needed.

--- app/components/ScrollToTop.tsx
+++ app/components/ScrollToTop.tsx
@@ -40,7 +40,8 @@ export const ScrollToTop = () => {
 							theme === 'light'
 								? 'bg-white bg-opacity-70 hover:bg-opacity-90 focus:ring-gray-500 text-black border-[1px] border-black'
 								: 'bg-black bg-opacity-70 hover:bg-opacity-90 focus:ring-gray-300 text-white border-[1px] border-white',
-							'inline-flex items-center rounded-full p-2 shadow-sm transition-all duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 backdrop-blur-md backdrop-filter'
+							'inline-flex items-center rounded-full p-2 shadow-sm transition-all duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 backdrop-blur-md backdrop-filter',
+							'block lg:hidden'
 						)}
 						initial={{ opacity: 0, scale: 0.8 }}
 						animate={{ opacity: 0.7, scale: 1 }}
