e7c8260d2b2f81912ca12e4672d7f620026a13f3
Nicholas Adamou <nicholasadamouemail@gmail.com>
2024-11-07 18:36:28 -0500

    Fix formatting issues in next.config.js
    Removed empty `experimental` object and standardized indentation. Cleaned up trailing commas to adhere to consistent styling rules. Adjusted file to ensure better readability and maintainability.

--- next.config.js
+++ next.config.js
@@ -2,26 +2,24 @@ const { withContentlayer } = require("next-contentlayer");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    serverActions: true,
-  },
-  reactStrictMode: true,
-  swcMinify: true,
-  images: {
-    domains: [
-      "tailwindui.com",
-      "images.unsplash.com",
-      "cdn.dribbble.com",
-      "m.media-amazon.com",
-      "ik.imagekit.io",
-      "miro.medium.com",
-      "img.clerk.com",
-      "i.ytimg.com",
-      "store.storeimages.cdn-apple.com",
-      "www.apple.com",
-    ],
-  },
-	pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"]
+	experimental: {},
+	reactStrictMode: true,
+	swcMinify: true,
+	images: {
+		domains: [
+			"tailwindui.com",
+			"images.unsplash.com",
+			"cdn.dribbble.com",
+			"m.media-amazon.com",
+			"ik.imagekit.io",
+			"miro.medium.com",
+			"img.clerk.com",
+			"i.ytimg.com",
+			"store.storeimages.cdn-apple.com",
+			"www.apple.com",
+		],
+	},
+	pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
 };
 
 module.exports = withContentlayer(nextConfig);
