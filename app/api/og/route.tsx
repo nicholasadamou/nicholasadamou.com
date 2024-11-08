59d66c3594711cd3b0f13c3ceaa74ba6dde619d9
Nicholas Adamou <nicholasadamouemail@gmail.com>
2024-11-07 19:09:13 -0500

    Fix query parameter retrieval in OG route
    Removed redundant fallback query parameters for 'type' and 'image' in OG route. Added logging for search parameters to debug request inputs.

--- app/api/og/route.tsx
+++ app/api/og/route.tsx
@@ -109,11 +109,13 @@ const createImageResponse = (
 export async function GET(req: NextRequest) {
 	const { searchParams } = req.nextUrl;
 
+	console.log(searchParams);
+
 	const title = searchParams.get("title") ?? "Default Title"; // Fallback title
 	const description = searchParams.get("description") ?? ""; // Fallback description
-	const type = searchParams.get("type") ?? searchParams.get("amp;type") ?? "note"; // 'note', 'project', 'homepage'
+	const type = searchParams.get("type") ?? "note"; // 'note', 'project', 'homepage'
 	const fontSize = searchParams.get("fontSize") ?? "64px"; // Optional font size
-	const image = searchParams.get("image") ?? searchParams.get("amp;image") ?? ""; // Optional image URL
+	const image = searchParams.get("image") ?? ""; // Optional image URL
 
 	let headerText = "";
 	let maxWidth = "90%";
