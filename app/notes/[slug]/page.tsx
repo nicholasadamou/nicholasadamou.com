fd1463f2f35dda25a62958d283c6013292e93838
Nicholas Adamou <nicholasadamouemail@gmail.com>
2024-12-26 12:07:53 -0500

    Add `generateMetadata` to pages and restore `"use client"`
    Adds the `generateMetadata` export to `notes` and `projects` pages for dynamic metadata generation. Also restores the `"use client"` directive in necessary components to ensure client-side rendering where required.

--- app/notes/[slug]/page.tsx
+++ app/notes/[slug]/page.tsx
@@ -1,7 +1,8 @@
-"use client";
-
 import { allNotes } from "contentlayer/generated";
 import ContentPage from "@/app/components/ContentPage";
+
+import {generateMetadata} from "./metadata";
+export {generateMetadata};
 
 export default function NotePage({ params }: { params: { slug: string } }) {
 	const note = allNotes.find((n) => n.slug === params.slug);
--- app/projects/[slug]/page.tsx
+++ app/projects/[slug]/page.tsx
@@ -1,7 +1,8 @@
-"use client";
-
 import { allProjects } from "contentlayer/generated";
 import ContentPage from "@/app/components/ContentPage";
+
+import {generateMetadata} from "./metadata";
+export {generateMetadata};
 
 export default function ProjectPage({ params }: { params: { slug: string } }) {
 	const project = allProjects.find((p) => p.slug === params.slug);
