59cce0a7736d36206268fc2afeef0171eb6610df
Nicholas Adamou <nicholasadamouemail@gmail.com>
2024-12-26 08:10:38 -0500

    fixed light theme of CommitHashLink

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,10 +1,10 @@
 'use client'
 
 import Link from "next/link"
-import React, { useEffect, useState } from "react"
-import { Github, GitCommit } from 'lucide-react'
-import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/app/components/ui/tooltip"
+import React from "react"
+import { Github } from 'lucide-react'
 import { motion } from "framer-motion"
+import CommitHashLink from "@/app/components/CommitHashLink"
 
 const footerVariants = {
 	hidden: { opacity: 0, y: 20 },
@@ -30,26 +30,6 @@ const childVariants = {
 }
 
 export function Footer(): React.JSX.Element {
-	const [commitHash, setCommitHash] = useState<string | null>(null)
-
-	useEffect(() => {
-		const fetchCommitHash = async () => {
-			try {
-				const response = await fetch('/api/commit')
-				if (response.ok) {
-					const data = await response.json()
-					setCommitHash(data.commitHash)
-				} else {
-					console.error('Failed to fetch commit hash')
-				}
-			} catch (error) {
-				console.error('Error fetching commit hash:', error)
-			}
-		}
-
-		fetchCommitHash()
-	}, [])
-
 	return (
 		<motion.footer
 			className="absolute bottom-0 inset-x-0 px-8 md:px-4 max-w-[700px] mx-auto flex flex-row gap-2 mt-32 py-6 w-full shrink-0 items-center justify-between"
@@ -61,31 +41,7 @@ export function Footer(): React.JSX.Element {
 				&copy; {new Date().getFullYear()} Nicholas Adamou.
 			</motion.p>
 			<motion.nav className="sm:ml-auto flex gap-4 sm:gap-6 items-center" variants={childVariants}>
-				{commitHash && (
-					<TooltipProvider>
-						<Tooltip>
-							<TooltipTrigger asChild>
-								<motion.div
-									whileHover={{ scale: 1.05 }}
-									whileTap={{ scale: 0.95 }}
-								>
-									<Link
-										className="text-sm transition-colors duration-200 text-secondary flex items-center gap-1 bg-[#191919] py-1 px-2 rounded-full"
-										href={`https://github.com/nicholasadamou/nicholasadamou.com/commit/${commitHash}`}
-										target="_blank"
-										rel="noopener noreferrer"
-									>
-										<GitCommit className="w-4 h-4" />
-										<span className="font-mono">{commitHash.slice(0, 7)}</span>
-									</Link>
-								</motion.div>
-							</TooltipTrigger>
-							<TooltipContent>
-								<p>View latest commit</p>
-							</TooltipContent>
-						</Tooltip>
-					</TooltipProvider>
-				)}
+				<CommitHashLink />
 				<motion.div
 					whileHover={{ scale: 1.1 }}
 					whileTap={{ scale: 0.95 }}
