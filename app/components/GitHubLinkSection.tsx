ba7e18ebc8b9fc1583c96472166ac2ea11160a8a
Nicholas Adamou <nicholasadamouemail@gmail.com>
2025-01-19 15:04:45 -0500

    Update styles for GitHubLinkSection buttons
    Revised button styles to improve contrast and consistency across light and dark themes. Adjusted hover states for better visual feedback and harmonized text and background colors. No functional changes were made to the component.

--- app/components/GitHubLinkSection.tsx
+++ app/components/GitHubLinkSection.tsx
@@ -5,29 +5,29 @@ import { Download, Github } from "lucide-react";
 import { LinkButton } from "@/app/components/mdx/LinkButton";
 
 interface GitHubLinkSectionProps {
-  url: string;
+	url: string;
 	zip: string;
 }
 
 const GitHubLinkSection: React.FC<GitHubLinkSectionProps> = ({ url, zip }) => {
-  return (
-    <section className="flex gap-3">
-      <LinkButton
-        href={url || ""}
-        icon={Github}
-        className="border border-[#191919] bg-[#191919] text-white hover:bg-slate-900 dark:bg-slate-50 dark:text-slate-800 dark:hover:bg-transparent dark:hover:text-slate-50"
-      >
-        GitHub
-      </LinkButton>
-      <LinkButton
-        href={zip || ""}
-        icon={Download}
-        className="border border-[#efefef] bg-transparent hover:bg-[#efefef] dark:border-[#191919] dark:text-slate-100 dark:hover:bg-slate-50 dark:hover:text-slate-900"
-      >
-        Download as zip
-      </LinkButton>
-    </section>
-  );
+	return (
+		<section className="flex gap-3">
+			<LinkButton
+				href={url || ""}
+				icon={Github}
+				className="border border-[#191919] bg-[#191919] text-[#efefef] hover:bg-[#efefef] hover:text-[#191919] dark:bg-[#efefef] dark:text-[#191919] dark:hover:bg-[#191919] dark:hover:text-[#efefef]"
+			>
+				GitHub
+			</LinkButton>
+			<LinkButton
+				href={zip || ""}
+				icon={Download}
+				className="border border-[#efefef] bg-transparent text-[#191919] hover:bg-[#efefef] hover:text-[#191919] dark:border-[#191919] dark:text-[#efefef] dark:hover:bg-[#191919] dark:hover:text-[#efefef]"
+			>
+				Download as zip
+			</LinkButton>
+		</section>
+	);
 };
 
 export default GitHubLinkSection;
